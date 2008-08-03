5844733f14e14cbdb5209112d789b72dfe66cf1d
Mathias Verraes <mathias@verraes.net>
2008-08-03 23:43:05 +0000

    Missing parts of 0.6 release branch

---
+++ tests/phpunit/koowa_unit_testing/tests/KObjectTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @version     $Id: KObjectTest.php 128 2008-03-18 01:19:19Z mjaz $
+ * @package     Koowa_Tests
+ * @copyright   Copyright (C) 2007 - 2008 Joomlatools. All rights reserved.
+ * @license     GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+ * @link        http://www.koowa.org
+ */
+
+/**
+ * KObjectTest
+ *
+ * @author      Mathias Verraes <mathias@joomlatools.org>
+ * @package     Koowa_Tests
+ */
+class KObjectTest extends PHPUnit_Framework_TestCase
+{
+    public function testGet() {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+}
+
