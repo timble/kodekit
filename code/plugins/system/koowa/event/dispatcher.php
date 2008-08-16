376fa83933791ef9fa9620980d952b9920f2394f
Johan Janssens <johan@timble.net>
2008-08-16 14:53:10 +0000

    Initial import

---
+++ code/plugins/system/koowa/event/dispatcher.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     Koowa_Event
+ * @copyright   Copyright (C) 2007 - 2008 Joomlatools. All rights reserved.
+ * @license     GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+ * @link        http://www.koowa.org
+ */
+
+/**
+ * Class to handle dispatching of events.
+ *
+ * @author Johan Janssens <johan@joomlatools.org>
+ * @package Koowa_Event
+ */
+class KEventDispatcher extends KPatternObservable
+{
+	/**
+	 * Registers an event handler to the event dispatcher
+	 *
+	 * @param	string	$handler	Name of the event handler
+	 */
+	public function register($handler)
+	{
+		if (class_exists($handler)) {
+			$this->attach(new $handler($this));
+		}
+	}
+
+	/**
+	 * Triggers an event by dispatching arguments to all observers that handle
+	 * the event and returning their return values.
+	 *
+	 * @param	string	$event			The event name
+	 * @param	array	$args			An array of arguments
+	 * @return	array	An array of results from each function call
+	 */
+	public function trigger($event, $args = array())
+	{
+		// Initialize variables
+		$result = array ();
+
+		/*
+		 * Iterate through all of the registered observers and trigger the event for 
+		 * each observer that handles the event.
+		 */
+		foreach ($this->_observers as $observer)
+		{
+			if (method_exists($observer, $event)) {
+				$result[] = $observer->update($event, $args);
+			}
+		}
+		return $result;
+	}
+}
---
+++ code/plugins/system/koowa/event/handler.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     Koowa_Event
+ * @copyright   Copyright (C) 2007 - 2008 Joomlatools. All rights reserved.
+ * @license     GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+ * @link        http://www.koowa.org
+ */
+
+/**
+ * Class to handle events.
+ *
+ * @author Johan Janssens <johan@joomlatools.org>
+ * @package Koowa_Event
+ */
+class KEventHandler extends KObserver
+{
+	/**
+	 * Method to trigger events
+	 *
+	 * @param array Arguments
+	 * @return mixed Routine return value
+	 */
+	public function update($event, $args = array())
+	{
+		if (method_exists($this, $event)) {
+			return call_user_func_array ( array($this, $event), $args );
+		} 
+		
+		return null;
+	}
+}
