05524589804beebe11ee5119b8b8a3f6d590c259
Johan Janssens <johan@timble.net>
2008-08-16 01:11:41 +0000

    Added svn properties

--- code/plugins/system/koowa/application/abstract.php
+++ code/plugins/system/koowa/application/abstract.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* @version $Id:  $
+* @version $Id$
 * @package Koowa_Application
 * @copyright Copyright (C) 2007 - 2008 Joomlatools. All rights reserved.
 * @license GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
