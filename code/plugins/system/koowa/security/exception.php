22dc8da98519486cdbf7c7b1a3dd6baaa9c7a6a0
Johan Janssens <johan@timble.net>
2008-08-21 20:40:30 +0000

    Added exception class

---
+++ code/plugins/system/koowa/security/exception.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     Koowa_Security
+ * @copyright   Copyright (C) 2007 - 2008 Joomlatools. All rights reserved.
+ * @license     GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+ * @link        http://www.koowa.org
+ */
+
+/**
+ * Koowa Database Exception class
+ *
+ * @author      Johan Janssens <johan@joomlatools.org>
+ * @package     Koowa_Security
+ */
+class KSecurityException extends Exception {}
