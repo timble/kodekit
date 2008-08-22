421d26548b17be44ddffa52516938d6b4b8bf862
Mathias Verraes <mathias@verraes.net>
2008-08-22 19:23:20 +0000

    Added CHANGELOG, LICENSE and README files

---
+++ code/plugins/system/koowa/CHANGELOG.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* @version      $Id:koowa.php 251 2008-06-14 10:06:53Z mjaz $
+* @package      Koowa
+* @copyright    Copyright (C) 2007 - 2008 Joomlatools. All rights reserved.
+* @license      GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+* @link     	http://www.koowa.org
+*/
+defined('_JEXEC') or die('Restricted access');
+?>
+
+Legend:
+
+ * -> Security Fix
+ # -> Bug Fix
+ $ -> Language fix or change
+ + -> Addition
+ ^ -> Change
+ - -> Removed
+ ! -> Note
+
+2008-08-22 Mathias Verraes
+ + Added changelog, license and readme 
