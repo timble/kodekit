23700eea7d33facb69ca92ec7f51cbe5d34dbdd8
Johan Janssens <johan@timble.net>
2008-08-27 21:44:45 +0000

    Fixed #6 KFactory creates multiple instances of same object

---
+++ code/plugins/system/koowa/factory/adapter/interface.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @version 	$Id:factory.php 46 2008-03-01 18:39:32Z mjaz $
+ * @package		Koowa_Factory
+ * @copyright	Copyright (C) 2007 - 2008 Joomlatools. All rights reserved.
+ * @license		GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+ */
+
+/**
+ * KFactoryAdpater for the Joomla! framework
+ *
+ * @author		Johan Janssens <johan@joomlatools.org>
+ * @package     Koowa_Factory
+ */
+interface KFactoryAdapterInterface
+{
+	/**
+	 * Create a object handle based on a class identifier
+	 *
+	 * @param mixed  $string 	The class identifier
+	 *
+	 * @return string|false
+	 */
+	public function createHandle($identifier);
+
+	/**
+	 * Create an object instance based on a class identifier
+	 *
+	 * @param mixed  $string 	The class identifier
+	 * @param array  $options 	An optional associative array of configuration settings.
+	 *
+	 * @return object
+	 */
+	public function createInstance($identifier, $options);
+}
---
+++ code/plugins/system/koowa/factory/chain.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @version		$Id:proxy.php 46 2008-03-01 18:39:32Z mjaz $
+ * @package		Koowa_Pattern
+ * @subpackage	Command
+ * @copyright	Copyright (C) 2007 - 2008 Joomlatools. All rights reserved.
+ * @license		GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+ * @link     	http://www.koowa.org
+ */
+
+/**
+ * Factory Command Chain
+ *
+ * The factory chain overrides the run method to be able to halt the chain
+ * when a 
+ *
+ * @author		Johan Janssens <johan@joomlatools.org>
+ * @package     Koowa_Factory
+ */
+class KFactoryChain extends KPatternCommandChain
+{
+  	/**
+	 * Run the commands in the chain
+	 * 
+	 * If a command returns not false the exection is halted
+	 * 
+	 * @param string  $name		The command name
+	 * @param object  $args		The command arguments
+	 *
+	 * @return string|false
+	 */
+  	public function run( $name, $args )
+  	{
+  		$iterator = $this->_priority->getIterator();
+
+		while($iterator->valid()) 
+		{
+    		$cmd = $this->_command[ $iterator->key()];
+   
+			$result = $cmd->execute( $name, $args );
+    		if ($result !== false) {
+      			return $result; //halt execution and return result
+      		}
+
+    		$iterator->next();
+		}
+		
+		return false;
+  	}
+}
