c336e0251b1d09d3ffd47b503aed05a4128cd614
Mathias Verraes <mathias@verraes.net>
2008-09-13 15:20:27 +0000

    Added KFilterInternalurl (WIP)

---
+++ code/plugins/system/koowa/filter/internalurl.php
@@ -0,0 +1,46 @@
+<?php
+/**
+* @version      $Id:koowa.php 251 2008-06-14 10:06:53Z mjaz $
+* @category		Koowa
+* @package      Koowa_Filter
+* @copyright    Copyright (C) 2007 - 2008 Joomlatools. All rights reserved.
+* @license      GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+*/
+
+/**
+ * Internal url filter
+ * 
+ * Check if an refers to a legal Joomla URL inside the system. Use when 
+ * redirecting to an URL that was passed in a request
+ *
+ * @todo		Do a proper implementation, see NookuFilterEditlink for ideas
+ * 
+ * @author		Mathias Verraes <mathias@joomlatools.org>
+ * @category	Koowa
+ * @package     Koowa_Filter
+ */
+class KFilterInternalurl extends KObject implements KFilterInterface
+{
+	/**
+	 * Validate a variable
+	 *
+	 * @param	mixed	Variable to be validated
+	 * @return	bool	True when the variable is valid
+	 */
+	public function validate($var)
+	{
+		return is_string($var) && JURI::isInternal($var);
+	}
+	
+	/**
+	 * Sanitize a variable
+	 *
+	 * @param	mixed	Variable to be sanitized
+	 * @return	string
+	 */
+	public function sanitize($var)
+	{
+		die('Not implemented:' .__METHOD__);
+	}
+}
+
