ad67c1d639058aa4b4c41c187bf685544ae64530
Johan Janssens <johan@timble.net>
2008-10-01 02:23:53 +0000

    Added $get, and String:parseQueryString and String:parseUri functions.

--- code/media/plg_koowa/js/koowa.js
+++ code/media/plg_koowa/js/koowa.js
@@ -1,8 +1,8 @@
-// <?php !! This fools phpdocumentor into parsing this file
 /**
  * @version		$Id$
  * @category    Koowa
  * @package     Koowa_Media
+ * @subpackage  Javascript
  * @copyright	Copyright (C) 2007 - 2008 Joomlatools. All rights reserved.
  * @license		GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
  * @link     	http://www.koowa.org
@@ -18,3 +18,34 @@ function KTableOrdering( order, dir, task )
 	submitform( task );
 }
 
+function $get(key)
+{
+	if(key == "") return;
+	
+	 var uri   = location.search.parseUri();
+	 var query = uri['query'].parseQueryString();
+	 return query[key]; 
+}	
+
+String.extend({
+ 
+	parseQueryString: function() 
+	{
+		var vars = this.split(/[&;]/);
+		var rs = {};
+		if (vars.length) vars.each(function(val) {
+			var keys = val.split('=');
+			if (keys.length && keys.length == 2) rs[keys[0]] = encodeURIComponent(keys[1]);
+		});
+		
+		return rs;
+	},
+ 
+	parseUri: function()
+	{
+		var bits = this.match(/^(?:([^:\/?#.]+):)?(?:\/\/)?(([^:\/?#]*)(?::(\d*))?)((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[\?#]|$)))*\/?)?([^?#\/]*))?(?:\?([^#]*))?(?:#(.*))?/);
+		return (bits)
+			? bits.associate(['uri', 'scheme', 'authority', 'domain', 'port', 'path', 'directory', 'file', 'query', 'fragment'])
+			: null;
+	}
+});
