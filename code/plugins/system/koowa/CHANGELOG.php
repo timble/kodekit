ac74360491a173be912d6029581f0a2add335b6a
Mathias Verraes <mathias@verraes.net>
2008-10-06 06:19:33 +0000

    GET forms no longer attach a token to the URL

--- code/plugins/system/koowa/CHANGELOG.php
+++ code/plugins/system/koowa/CHANGELOG.php
@@ -20,6 +20,9 @@ Legend:
  - -> Removed
  ! -> Note
 
+2008-10-06 Mathias Verraes
+ ^ GET forms no longer attach a token to the URL
+
 2008-10-04 Mathias Verraes
  - Deprecated REQUEST hash, use KInput::get('varname', array('post', 'get'), $filter);
  + KInput::get() accepts an array of hash names as the second parameter
