a93cf43d45d34bf9d3ab549c2b2876bd974a4ef4
Mathias Verraes <mathias@verraes.net>
2009-07-02 22:40:25 +0000

    docblocks

--- code/plugins/system/koowa/factory/adapter/component/identifier.php
+++ code/plugins/system/koowa/factory/adapter/component/identifier.php
@@ -1,6 +1,24 @@
 <?php
+/**
+ * @version 	$Id$
+ * @category	Koowa
+ * @package		Koowa_Factory
+ * @subpackage 	Adapter
+ * @copyright	Copyright (C) 2007 - 2009 Johan Janssens and Mathias Verraes. All rights reserved.
+ * @license		GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+ */
 
-
+/**
+ * Identifier for a component
+ *
+ * Wraps identifiers of the form application::extension.component.type[[.path].name]
+ * in an object, providing public accessors and methods for derived formats
+ *
+ * @author		Mathias Verraes <mathias@koowa.org>
+ * @category	Koowa
+ * @package     Koowa_Factory
+ * @subpackage 	Adapter
+ */
 class KFactoryAdapterComponentIdentifier extends KObject
 {
 	/**
