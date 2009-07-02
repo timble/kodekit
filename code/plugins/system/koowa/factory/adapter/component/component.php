7dd2ecf5deb946d0b0aaeb80dc2cd9298fdf815d
Mathias Verraes <mathias@verraes.net>
2009-07-02 22:59:59 +0000

    typos

--- code/plugins/system/koowa/factory/adapter/component/component.php
+++ code/plugins/system/koowa/factory/adapter/component/component.php
@@ -76,9 +76,9 @@ class KFactoryAdapterComponent extends KFactoryAdapterAbstract
 			//Create the name suffix.
 			$suffix = !empty($identifier->path) ? strtolower($identifier->path).'_'.$identifier->name : $identifier->name;
 
-			// @todo, we shouldnt need this option anymore
+			// @todo, we shouldnt need $options['name'] anymore
 			$options['name'] = array('prefix' => $identifier->component, 'base' => $identifier->type, 'suffix' => $suffix);
-			$option['identifier'] = $identifier;
+			$options['identifier'] = $identifier;
 
 			$instance = new $classname($options);
 		}
