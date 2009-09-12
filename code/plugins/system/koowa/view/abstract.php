b37ef87f038de0a2b8901d37dc4f4226a491fbff
Johan Janssens <johan@timble.net>
2009-09-12 15:46:39 +0000

    Fixed docblocks

--- code/plugins/system/koowa/view/abstract.php
+++ code/plugins/system/koowa/view/abstract.php
@@ -150,10 +150,10 @@ abstract class KViewAbstract extends KObject implements KFactoryIdentifiable
 	/**
 	 * Execute and echo's the views output
  	 *
-	 * @return 	this
+	 * @return KViewAbstract
 	 */
 	public function display()
-	{
+	{	
 		//Render the template
 		echo $this->loadTemplate();
 		return $this;
@@ -192,7 +192,7 @@ abstract class KViewAbstract extends KObject implements KFactoryIdentifiable
 	*
 	* </code>
 	*
-	* @return object KViewAbstract
+	* @return KViewAbstract
 	*/
 	public function assign()
 	{
@@ -257,8 +257,8 @@ abstract class KViewAbstract extends KObject implements KFactoryIdentifiable
    /**
 	* Sets the layout name to use
 	*
-	* @param	string 	$template The template name.
-	* @return 	object 	KViewAbstract
+	* @param	string 	The template name.
+	* @return 	KViewAbstract
 	*/
 	public function setLayout($layout)
 	{
@@ -269,8 +269,8 @@ abstract class KViewAbstract extends KObject implements KFactoryIdentifiable
 	 /**
      * Sets the _escape() callback.
      *
-     * @param 	mixed 	$spec The callback for _escape() to use.
-     * @return 	object 	KViewAbstract
+     * @param 	mixed The callback for _escape() to use.
+     * @return 	KViewAbstract
      */
     public function setEscape($spec)
     {
@@ -282,7 +282,7 @@ abstract class KViewAbstract extends KObject implements KFactoryIdentifiable
 	 * Adds to the stack of view script paths in LIFO order.
 	 *
 	 * @param string|array The directory (-ies) to add.
-	 * @return object KViewAbstract
+	 * @return  KViewAbstract
 	 */
 	public function addTemplatePath($path)
 	{
@@ -311,8 +311,8 @@ abstract class KViewAbstract extends KObject implements KFactoryIdentifiable
 	/**
 	 * Load a template file -- first look in the templates folder for an override
 	 *
-	 * @param string $tpl The name of the template source file ...
-	 * automatically searches the template paths and compiles as needed.
+	 * @param 	string 	The name of the template source file automatically searches 
+	 * 					the template paths and compiles as needed.
 	 * @throws KViewException
 	 * @return string The output of the the template script.
 	 */
