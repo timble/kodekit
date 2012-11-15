30e73a8364090b180f34dbd197a54c243abbee8c
Ercan Özkaya <ercan@timble.net>
2012-11-15 15:36:04 +0200

    re #44: Fix strict errors in KDatabaseRowset

--- code/libraries/koowa/database/rowset/abstract.php
+++ code/libraries/koowa/database/rowset/abstract.php
@@ -89,8 +89,12 @@ abstract class KDatabaseRowsetAbstract extends KObjectSet implements KDatabaseRo
      * @param  object   A KDatabaseRow object to be inserted
      * @return boolean	TRUE on success FALSE on failure
      */
-    public function insert(KDatabaseRowInterface $row)
+    public function insert(KObjectHandlable $row)
     {
+        if (!$row instanceof KDatabaseRowInterface) {
+            throw new InvalidArgumentException('Row needs to implement KDatabaseRowInterface');
+        }
+    	
         if(isset($this->_identity_column)) {
             $handle = $row->{$this->_identity_column};
         } else {
@@ -113,8 +117,12 @@ abstract class KDatabaseRowsetAbstract extends KObjectSet implements KDatabaseRo
      * @param  object   A KDatabaseRow object to be removed
      * @return KDatabaseRowsetAbstract
      */
-    public function extract(KDatabaseRowInterface $row)
+	public function extract(KObjectHandlable $row)
     {
+        if (!$row instanceof KDatabaseRowInterface) {
+            throw new InvalidArgumentException('Row needs to implement KDatabaseRowInterface');
+        }
+        
         if(isset($this->_identity_column)) {
            $handle = $row->{$this->_identity_column};
         } else {
