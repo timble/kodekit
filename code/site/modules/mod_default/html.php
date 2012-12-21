7dec46d0cb1c3ce4a3b0cf30772b08f3bd7ad50f
Ercan Özkaya <ercan@timble.net>
2012-12-21 19:11:48 +0200

    test #24: Cast module parameters from object to array before passing them to KConfig

--- code/site/modules/mod_default/html.php
+++ code/site/modules/mod_default/html.php
@@ -120,7 +120,7 @@ class ModDefaultHtml extends KViewHtml
                 }
             }
         }
-        else $params = json_decode($string);
+        else $params = (array) json_decode($string);
 
         $params = new KConfig($params);
         return $params;
