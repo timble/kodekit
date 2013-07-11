8a8dd98beaeb79068946ee4961ea9cbf00901f67
Ercan Ozkaya <ercan@timble.net>
2013-07-11 16:58:39 +0300

    re #79: Add base plugin

---
+++ code/administrator/components/com_default/plugins/finder.php
@@ -0,0 +1,514 @@
+<?php
+/**
+ * @package     Koowa
+ * @copyright   Copyright (C) 2011 - 2013 Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.joomlatools.com
+ */
+
+defined('JPATH_BASE') or die;
+
+jimport('joomla.application.component.helper');
+
+// Load the base adapter.
+require_once JPATH_ADMINISTRATOR . '/components/com_finder/helpers/indexer/adapter.php';
+
+/**
+ * Finder adapter for Koowa packages.
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  Finder.Content
+ * @since       2.5
+ */
+abstract class ComDefaultPluginFinder extends FinderIndexerAdapter
+{
+    /**
+     * Package name
+     * @var string
+     */
+    protected $package;
+
+    /**
+     * Resource that the plugin will act on
+     * @var string
+     */
+    protected $resource;
+
+    /**
+     * Model identifier/model object
+     * @var KServiceIdentifier
+     */
+    protected $model;
+
+    /**
+     * Model identifier/model object
+     * @var KServiceIdentifier
+     */
+    protected $category_model;
+
+    /**
+     * Array of instructions. These are used to tell the indexer to include certain properties
+     * and how important they are
+     * @var array
+     */
+    protected $instructions = array();
+
+    /**
+     * Constructor
+     *
+     * @param   object  &$subject  The object to observe
+     * @param   array   $config    An array that holds the plugin configuration
+     *
+     * @since   2.5
+     */
+    public function __construct(&$subject, $config)
+    {
+        if ($this->bootFramework())
+        {
+            $configuration = new KConfig();
+
+            $this->_initialize($configuration);
+
+            foreach ($configuration as $key => $value) {
+                $this->$key = KConfig::unbox($value);
+            }
+        }
+
+        parent::__construct($subject, $config);
+
+        $this->loadLanguage();
+    }
+
+    protected function _initialize(KConfig $config)
+    {
+        $config->append(array(
+            'package' => strtolower(substr(get_class($this), 9)) // Remove plgFinder from class name
+        ))->append(array(
+                'resource' => $config->package,
+        ))->append(array(
+            'layout'     => $config->resource,
+            'model'      => KInflector::pluralize($config->resource),
+            'category_model' => 'categories',
+            'context'    => $config->package,
+            'extension'  => 'com_'.$config->package,
+            'type_title' => ucfirst($config->resource),
+            'instructions' => array(
+                FinderIndexer::TITLE_CONTEXT => array('meta_title'),
+                FinderIndexer::TEXT_CONTEXT => array('description'),
+                FinderIndexer::META_CONTEXT => array('meta_keywords', 'meta_description'),
+                FinderIndexer::PATH_CONTEXT => array('slug')
+            )
+        ));
+    }
+
+    /**
+     * Method to remove the link information for items that have been deleted.
+     *
+     * @param   string  $context  The context of the action being performed.
+     * @param   JTable  $table    A JTable object containing the record to be deleted
+     *
+     * @return  boolean  True on success.
+     *
+     * @since   2.5
+     * @throws  Exception on database error.
+     */
+    public function onFinderAfterDelete($context, $table)
+    {
+        if ($context === $this->extension.'.'.$this->resource) {
+            $id = $table->id;
+        }
+        elseif ($context == 'com_finder.index') {
+            $id = $table->link_id;
+        }
+        else {
+            return true;
+        }
+
+        // Remove the items.
+        return $this->remove($id);
+    }
+
+    /**
+     * Method to determine if the access level of an item changed.
+     *
+     * @param   string   $context  The context of the content passed to the plugin.
+     * @param   JTable   $row      A JTable object
+     * @param   boolean  $isNew    If the content has just been created
+     *
+     * @return  boolean  True on success.
+     *
+     * @since   2.5
+     * @throws  Exception on database error.
+     */
+    public function onFinderAfterSave($context, $row, $isNew)
+    {
+        // We only want to handle articles here
+        if ($context == $this->extension.'.'.$this->resource)
+        {
+            if (!$isNew && $row->access_changed) {
+                $this->itemAccessChange($row);
+            }
+
+            // Reindex the item
+            $this->reindex($row->id);
+        }
+
+        if ($context == $this->extension.'.category')
+        {
+            if (!$isNew && $row->access_changed) {
+                $this->categoryAccessChange($row);
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Method to update the link information for items that have been changed
+     * from outside the edit screen. This is fired when the item is published,
+     * unpublished, archived, or unarchived from the list view.
+     *
+     * @param   string   $context  The context for the content passed to the plugin.
+     * @param   array    $pks      A list of primary key ids of the content that has changed state.
+     * @param   integer  $value    The value of the state that the content has been changed to.
+     *
+     * @return  void
+     *
+     * @since   2.5
+     */
+    public function onFinderChangeState($context, $pks, $value)
+    {
+        if ($context == $this->extension.'.'.$this->resource)
+        {
+            $this->itemStateChange($pks, $value);
+        }
+        // Handle when the plugin is disabled
+        if ($context == 'com_plugins.plugin' && $value === 0)
+        {
+            $this->pluginDisable($pks);
+        }
+    }
+
+    /**
+     * Method to update the item link information when the item category is
+     * changed. This is fired when the item category is published or unpublished
+     * from the list view.
+     *
+     * @param   string   $extension  The extension whose category has been updated.
+     * @param   array    $pks        A list of primary key ids of the content that has changed state.
+     * @param   integer  $value      The value of the state that the content has been changed to.
+     *
+     * @return  void
+     *
+     * @since   2.5
+     */
+    public function onFinderCategoryChangeState($extension, $pks, $value)
+    {
+        if ($extension == $this->extension) {
+            $this->categoryStateChange($pks, $value);
+        }
+    }
+
+    /**
+     * Main index function run when indexing happens
+     * @param FinderIndexerResult $item
+     * @return bool|void
+     */
+    protected function index(FinderIndexerResult $item)
+    {
+        // Check if the extension is enabled
+        if (JComponentHelper::isEnabled($this->extension) == false) {
+            return;
+        }
+
+        //Add the instructions
+        foreach ($this->instructions AS $type => $instructions)
+        {
+            foreach ($instructions AS $instruction) {
+                $item->addInstruction($type, $instruction);
+            }
+        }
+
+        // Add the type taxonomy data.
+        $item->addTaxonomy('Type', $this->type_title);
+
+        FinderIndexerHelper::getContentExtras($item);
+
+        // Index the item.
+        if (method_exists('FinderIndexer', 'getInstance')) {
+            FinderIndexer::getInstance()->index($item);
+        } else {
+            FinderIndexer::index($item);
+        }
+    }
+
+    /**
+     * Method to setup the indexer to be run.
+     *
+     * @return  boolean  True on success.
+     *
+     * @since   2.5
+     */
+    protected function setup()
+    {
+        if (!$this->bootFramework()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function bootFramework()
+    {
+        // This is useful in CLI mode
+        if (!class_exists('Koowa'))
+        {
+            if (!defined('JDEBUG')) {
+                define('JDEBUG', 0);
+            }
+
+            JPluginHelper::importPlugin('system');
+
+            JDispatcher::getInstance()->trigger('onAfterInitialiase');
+        }
+
+        return class_exists('Koowa');
+    }
+
+    /**
+     * Method to update index data on category access level changes
+     *
+     * @param   JTable  $category  A JTable object
+     *
+     * @return  void
+     *
+     * @since   2.5
+     */
+    protected function categoryAccessChange($category)
+    {
+        $rowset = $this->getModel()->category($category->id)->getList();
+
+        // Adjust the access level for each item within the category.
+        foreach ($rowset as $row)
+        {
+            // Set the access level.
+            $temp = max($row->access, $category->new_access);
+
+            // Update the item.
+            $this->change((int) $row->id, 'access', $temp);
+
+            // Reindex the item
+            $this->reindex($row->id);
+        }
+    }
+
+    /**
+     * Method to update index data on category access level changes
+     *
+     * @param   array    $pks    A list of primary key ids of the content that has changed state.
+     * @param   integer  $value  The value of the state that the content has been changed to.
+     *
+     * @return  void
+     *
+     * @since   2.5
+     */
+    protected function categoryStateChange($pks, $value)
+    {
+        // The item's published state is tied to the category
+        // published state so we need to look up all published states
+        // before we change anything.
+        foreach ($pks as $pk)
+        {
+            $rowset = $this->getModel()->category($pk)->getList();
+
+            // Adjust the state for each item within the category.
+            foreach ($rowset as $row)
+            {
+                $temp = $value;
+
+                // Update the item.
+                $this->change($row->id, 'state', $temp);
+
+                // Reindex the item
+                $this->reindex($row->id);
+            }
+        }
+    }
+
+    /**
+     * Method to get the number of content items available to index.
+     *
+     * @return  integer  The number of content items available to index.
+     *
+     * @since   2.5
+     * @throws  Exception on database error.
+     */
+    protected function getContentCount()
+    {
+        return $this->getModel()->getTotal();
+    }
+
+    /**
+     * Method to get a content item to index.
+     *
+     * @param   integer  $id  The id of the content item.
+     *
+     * @return  FinderIndexerResult  A FinderIndexerResult object.
+     *
+     * @since   2.5
+     */
+    protected function getItem($id)
+    {
+        $row = $this->getModel()->id($id)->getItem();
+
+        return $this->getFinderItem($row);
+    }
+
+    /**
+     * Method to get a list of content items to index.
+     *
+     * @param   integer         $offset  The list offset.
+     * @param   integer         $limit   The list limit.
+     * @param   JDatabaseQuery  $query   A JDatabaseQuery object. [optional]
+     *
+     * @return  array  An array of FinderIndexerResult objects.
+     *
+     * @since   2.5
+     * @throws  Exception on database error.
+     */
+    protected function getItems($offset, $limit, $query = null)
+    {
+        $rowset = $this->getModel()
+            ->limit($limit)
+            ->offset($offset)
+            ->getList();
+
+        $results = array();
+        foreach ($rowset AS $row) {
+            $results[] = $this->getFinderItem($row);
+
+        }
+
+        return $results;
+    }
+
+    /**
+     * Returns the model
+     *
+     * @return KModelAbstract
+     */
+    protected function getModel()
+    {
+        if (!$this->model instanceof KModelAbstract)
+        {
+            if (strpos($this->model, '.') === false) {
+                $this->model = 'com://admin/'.$this->package.'.model.'.$this->model;
+            }
+
+            $this->model = KService::get($this->model);
+        }
+
+        return $this->model;
+    }
+
+    /**
+     * Returns the category model
+     *
+     * @return KModelAbstract
+     */
+    protected function getCategoryModel()
+    {
+        if (!$this->category_model instanceof KModelAbstract)
+        {
+            if (strpos($this->category_model, '.') === false) {
+                $this->category_model = 'com://admin/'.$this->package.'.model.'.$this->category_model;
+            }
+
+            $this->category_model = KService::get($this->category_model);
+        }
+
+        return $this->category_model;
+    }
+
+    /**
+     * Turns a standard KDatabaseRow into a finder item
+     * @param $row
+     * @return object
+     */
+    protected function getFinderItem(KDatabaseRowInterface $row)
+    {
+        $data = $row->getData();
+
+        //Get the indexer result item
+        $item = JArrayHelper::toObject($data, 'FinderIndexerResult');
+
+        $item->url = $this->getURL($item->id, $this->extension, $this->layout);
+        $item->route = $this->getLink($row);
+        $item->path = FinderIndexerHelper::getContentPath($item->route);
+
+        if ($item->description) {
+            // Trigger the onContentPrepare event.
+            $item->summary = FinderIndexerHelper::prepareContent($item->description, $item->params);
+        }
+
+        // Translate the state. Articles should only be published if the category is published.
+        $item->state = min($item->enabled, $item->category_enabled);
+
+        // Set the item type.
+        $item->type_id = $this->type_id;
+
+        // Set the mime type.
+        $item->mime = $this->mime;
+
+        // Set the item layout.
+        $item->layout = $this->layout;
+
+        // Set the extension if present
+        if (isset($row->extension)) {
+            $item->extension = $row->extension;
+        }
+
+        return $item;
+    }
+
+    /**
+     * Method to update index data on published state changes
+     *
+     * @param   array    $pks    A list of primary key ids of the content that has changed state.
+     * @param   integer  $value  The value of the state that the content has been changed to.
+     *
+     * @return  void
+     *
+     * @since   2.5
+     */
+    protected function itemStateChange($pks, $value)
+    {
+        foreach ($pks as $pk)
+        {
+            // Update the item.
+            $this->change($pk, 'state', $value);
+
+            // Reindex the item
+            $this->reindex($pk);
+        }
+    }
+
+    /**
+     * Method to update index data on access level changes
+     *
+     * @param   JTable  $row  A JTable object
+     *
+     * @return  void
+     *
+     * @since   2.5
+     */
+    protected function itemAccessChange($row)
+    {
+        $this->change((int) $row->id, 'access', $row->new_access);
+    }
+
+
+    protected function getLink(KDatabaseRowInterface $row) {
+        return sprintf('index.php?option=%s&view=%s&slug=%s', $this->extension, $this->resource, $row->slug);
+    }
+}
