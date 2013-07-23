298b7332b407873058265131fecde292716dd783
Ercan Ozkaya <ercan@timble.net>
2013-07-23 14:40:32 +0300

    test #23: Add our own exception renderer

--- code/plugins/system/koowa/koowa.php
+++ code/plugins/system/koowa/koowa.php
@@ -50,7 +50,9 @@ class plgSystemKoowa extends JPlugin
 		define('KDEBUG', JDEBUG);
 
         //Set exception handler
-		set_exception_handler(array($this, 'exceptionHandler'));
+        if (JDEBUG) {
+            set_exception_handler(array($this, 'exceptionHandler'));
+        }
 
 		// Koowa: setup
         $path = JPATH_LIBRARIES.'/koowa/libraries/koowa.php';
@@ -140,55 +142,45 @@ class plgSystemKoowa extends JPlugin
 	}
 
  	/**
-	 * Catch all exception handler
+	 * Custom exception handler
 	 *
-	 * Calls the Joomla error handler to process the exception
-	 *
-	 * @param object an Exception object
+	 * @param Exception $exception an Exception object
 	 * @return void
 	 */
-	public function exceptionHandler($exception)
+	public function exceptionHandler(Exception $exception)
 	{
-		$this->_exception = $exception; //store the exception for later use
+        try
+        {
+            // If Koowa does not exist let Joomla handle the exception
+            if (!class_exists('Koowa') || !class_exists('ComKoowaTemplateError')) {
+                throw new Exception('');
+            }
 
-		if ($exception instanceof KException || $exception instanceof JException) {
-            $this->errorHandler($exception);
+            $data = array(
+                'exception' => $exception
+            );
+
+            $template = KService::get('com:koowa.template.error');
+            $template->addFilter(array('shorttag', 'variable'));
+            $template->loadFile(
+                JPATH_ROOT.'/libraries/koowa/components/com_koowa/views/error/tmpl/default.php',
+                $data
+            );
+
+            while (@ob_end_clean());
+
+            header('Content-Type: text/html');
+            echo $template->render();
+
+            exit;
         }
-		//Change the Joomla error handler to our own local handler and call it
-		JError::setErrorHandling( E_ERROR, 'callback', array($this,'errorHandler'));
-
-		//Make sure we have a valid status code
-		JError::raiseError(KHttpResponse::isError($exception->getCode()) ? $exception->getCode() : 500, $exception->getMessage());
-	}
-
-	/**
-	 * Custom JError callback
-	 *
-	 * Push the exception call stack in the JException returned through the call back
-	 * adn then rener the custom error page
-	 *
-	 * @param object A JException object
-	 * @return void
-	 */
-	public function errorHandler($error)
-	{
-		$error->setProperties(array(
-			'backtrace'	=> $this->_exception->getTrace(),
-			'file'		=> $this->_exception->getFile(),
-			'line'		=> $this->_exception->getLine()
-		));
-
-		$debug = JFactory::getConfig()->get('debug');
-
-	    if($debug) {
-			$error->set('message', (string) $this->_exception);
-		} else {
-			$error->set('message', KHttpResponse::getMessage($error->get('code')));
-		}
-
-		//Make sure the buffers are cleared
-		while(@ob_get_clean());
-
-		JError::customErrorPage($error);
+        catch (Exception $e)
+        {
+            if (version_compare(JVERSION, '3.0', '>=')) {
+                JErrorPage::render($exception);
+            } else {
+                JError::raiseError($exception->getCode(), $exception->getMessage());
+            }
+        }
 	}
 }
