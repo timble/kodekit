fee0de3e15907b02b3e50b5c4894a3532e2644a7
Ercan Ozkaya <ercan@timble.net>
2013-08-09 13:45:20 +0300

    Rename bootstrap-x.css files to x.css since they don't contain bootstrap code anymore

--- code/libraries/koowa/components/com_koowa/templates/helpers/bootstrap.php
+++ code/libraries/koowa/components/com_koowa/templates/helpers/bootstrap.php
@@ -66,7 +66,7 @@ class ComKoowaTemplateHelperBootstrap extends ComKoowaTemplateHelperBehavior
             'javascript'   => false,
             'wrapper'      => $identifier->type.'_'.$identifier->package,
             'package'      => $identifier->package,
-            'application'  => $identifier->type === 'mod' ? 'module' : $identifier->application,
+            'file'         => $identifier->type === 'mod' ? 'module' : $identifier->application,
             'load_default' => version_compare(JVERSION, '3.0', '<')
         ));
 
@@ -86,17 +86,15 @@ class ComKoowaTemplateHelperBootstrap extends ComKoowaTemplateHelperBehavior
             self::$_loaded['bootstrap-css'] = true;
         }
 
-        $filename = 'bootstrap'.($config->application ? '-'.$config->application : '');
-
-        if (!isset(self::$_loaded[$config->package.'-'.$filename]))
+        if (!isset(self::$_loaded[$config->package.'-'.$config->file]))
         {
             $template  = 'com_%s/css/%s.css';
             $try_files = array(
-                sprintf($template, $config->package, $filename)
+                sprintf($template, $config->package, $config->file)
             );
 
             if (version_compare(JVERSION, '3.0', '<')) {
-                array_unshift($try_files, sprintf($template, $config->package, $filename.'-25'));
+                array_unshift($try_files, sprintf($template, $config->package, $config->file.'-25'));
             }
 
             foreach ($try_files as $file)
@@ -105,7 +103,7 @@ class ComKoowaTemplateHelperBootstrap extends ComKoowaTemplateHelperBehavior
                 {
                     $html .= sprintf('<style src="media://%s" />', $file);
 
-                    self::$_loaded[$config->package.'-'.$filename] = true;
+                    self::$_loaded[$config->package.'-'.$config->file] = true;
 
                     break;
                 }
