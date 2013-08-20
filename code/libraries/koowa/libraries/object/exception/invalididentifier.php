8b0f5d47e8548c1c356944faaa2591e2db5a33e2
Johan Janssens <johan@timble.net>
2013-08-20 22:20:43 +0200

    re #91 : Added KObjectExceptionInvalidIdentifier

---
+++ code/libraries/koowa/libraries/object/exception/invalididentifier.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Koowa Framework - http://developer.joomlatools.com/koowa
+ *
+ * @copyright	Copyright (C) 2007 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://github.com/joomlatools/koowa for the canonical source repository
+ */
+
+
+/**
+ * Invalid Identifier Object Exception
+ *
+ * @author  Johan Janssens <https://github.com/johanjanssens>
+ * @package Koowa\Library\Object
+ */
+class KObjectExceptionInvalidIdentifier extends InvalidArgumentException implements KObjectException {}
