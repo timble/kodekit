50ac967a8f50f421dbb3e95b105c4ebd3e2f47c7
Ercan Ozkaya <ercan@timble.net>
2013-08-22 22:41:54 +0300

    re #91: Rename @template to @include in layout files

--- code/libraries/koowa/libraries/template/filter/alias.php
+++ code/libraries/koowa/libraries/template/filter/alias.php
@@ -10,7 +10,7 @@
 /**
  * Alias Template Filter
  *
- * Filter for aliases such as @template, @translate, @helper, @route etc
+ * Filter for aliases such as @include, @translate, @helper, @route etc
  *
  * @author  Johan Janssens <https://github.com/johanjanssens>
  * @package Koowa\Library\Template
@@ -28,7 +28,7 @@ class KTemplateFilterAlias extends KTemplateFilterAbstract implements KTemplateF
         '@date('        => '$this->renderHelper(\'date.format\',',
         '@overlay('     => '$this->renderHelper(\'behavior.overlay\', ',
         '@translate('   => '$this->translate(',
-        '@template('    => '$this->loadIdentifier(',
+        '@include('    => '$this->loadIdentifier(',
         '@route('       => '$this->getView()->createRoute(',
         '@escape('      => '$this->getView()->escape(',
     );
