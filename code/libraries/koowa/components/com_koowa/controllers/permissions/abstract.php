5fdfb0c9d60972330541402e2e08301dd199c159
Ercan Ozkaya <ercan@timble.net>
2013-09-10 18:23:40 +0200

    re #91: Add token checks to ComKoowaControllerBehaviorPermissible

--- code/libraries/koowa/components/com_koowa/controllers/permissions/abstract.php
+++ code/libraries/koowa/components/com_koowa/controllers/permissions/abstract.php
@@ -62,43 +62,4 @@ abstract class ComKoowaControllerPermissionAbstract extends KControllerPermissio
 
         return JFactory::getUser()->authorise('core.manage', 'com_'.$component) === true;
     }
-
-    /**
-     * Command handler
-     *
-     * Handles token validation
-     *
-     * {@inheritdoc}
-     */
-    public function canExecute($action)
-    {
-        if (!$this->_checkToken()) {
-            throw new KControllerExceptionForbidden('Invalid token or session time-out');
-        }
-
-        return parent::canExecute($action);
-    }
-
-    /**
-     * Check the token to prevent CSRF exploits
-     *
-     * @return  boolean Returns FALSE if the check failed. Otherwise TRUE.
-     */
-    protected function _checkToken()
-    {
-        if ($this->isDispatched())
-        {
-            $method = KRequest::method();
-
-            //Only check the token for PUT, DELETE and POST requests
-            if ($method != KHttpRequest::GET && $method != KHttpRequest::OPTIONS)
-            {
-                if (KRequest::token() !== JSession::getFormToken()) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
 }
