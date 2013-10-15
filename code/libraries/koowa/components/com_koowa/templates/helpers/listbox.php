ab81da62bcdfc1f65e5162a2877b34d113600f26
Arunas Mazeika <arunas@timble.net>
2013-10-15 09:48:48 +0200

    re #104 Bypass model querying if options are available.

--- code/libraries/koowa/components/com_koowa/templates/helpers/listbox.php
+++ code/libraries/koowa/components/com_koowa/templates/helpers/listbox.php
@@ -179,27 +179,31 @@ class ComKoowaTemplateHelperListbox extends ComKoowaTemplateHelperSelect
         }
         else
         {
-            $options = array();
-            if ($config->deselect) {
-                $options[] = $this->option(array('text' => $this->translate($config->prompt)));
-            }
-
-            $list = $this->getObject($config->identifier)->set($config->filter)->getList();
-
-            //Get the list of items
-            $items = $list->getColumn($config->value);
-            if ($config->unique) {
-                $items = array_unique($items);
-            }
-
-            foreach ($items as $key => $value)
+            if (!$config->options)
             {
-                $item      = $list->find($key);
-                $options[] = $this->option(array('text' => $item->{$config->text}, 'value' => $item->{$config->value}));
-            }
+                $options = array();
 
-            //Add the options to the config object
-            $config->options = $options;
+                if ($config->deselect) {
+                    $options[] = $this->option(array('text' => $this->translate($config->prompt)));
+                }
+
+                $list = $this->getObject($config->identifier)->set($config->filter)->getList();
+
+                //Get the list of items
+                $items = $list->getColumn($config->value);
+                if ($config->unique) {
+                    $items = array_unique($items);
+                }
+
+                foreach ($items as $key => $value)
+                {
+                    $item      = $list->find($key);
+                    $options[] = $this->option(array('text' => $item->{$config->text}, 'value' => $item->{$config->value}));
+                }
+
+                //Add the options to the config object
+                $config->options = $options;
+            }
 
             if ($config->select2) {
                 $html .= $this->_listboxSelect2($config);
