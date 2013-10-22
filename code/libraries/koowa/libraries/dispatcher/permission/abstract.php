de2ec6014d089f9c0186a87bbe84999429bdedca
Johan Janssens <johan@timble.net>
2013-10-22 15:33:15 +0200

    re #91 : Added KDispatcherPermission package

---
+++ code/libraries/koowa/libraries/dispatcher/permission/abstract.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Koowa Framework - http://developer.joomlatools.com/koowa
+ *
+ * @copyright	Copyright (C) 2007 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://github.com/joomlatools/koowa for the canonical source repository
+ */
+
+namespace Nooku\Library;
+
+/**
+ * Abstract Dispatcher Permission
+ *
+ * @author  Johan Janssens <https://github.com/johanjanssens>
+ * @package Koowa\Library\Dispatcher
+ */
+abstract class KDispatcherPermissionAbstract extends KObjectMixinAbstract implements KDispatcherPermissionInterface
+{
+    /**
+     * Permission handler for forward actions
+     *
+     * @return  boolean  Return TRUE if action is permitted. FALSE otherwise.
+     */
+    public function canForward()
+    {
+        return true;
+    }
+
+    /**
+     * Permission handler for dispatch actions
+     *
+     * @return  boolean  Return TRUE if action is permitted. FALSE otherwise.
+     */
+    public function canDispatch()
+    {
+        return true;
+    }
+
+    /**
+     * Permission handler for redirect actions
+     *
+     * @return  boolean  Return TRUE if action is permitted. FALSE otherwise.
+     */
+    public function canRedirect()
+    {
+        return true;
+    }
+
+    /**
+     * Permission handler for send actions
+     *
+     * @return  boolean  Return TRUE if action is permitted. FALSE otherwise.
+     */
+    public function canSend()
+    {
+        return true;
+    }
+}
