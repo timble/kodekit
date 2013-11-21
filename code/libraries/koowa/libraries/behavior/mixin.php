36b7195c2f20a084230b1960bf1c4de4af0ec440
Johan Janssens <johan@timble.net>
2013-11-21 15:24:35 +0100

    re #110 : Re-worked attachBehavior logic. Behaviors are now only attached if a behavior for that name does not exist yet.

--- code/libraries/koowa/libraries/behavior/mixin.php
+++ code/libraries/koowa/libraries/behavior/mixin.php
@@ -10,8 +10,8 @@
 /**
  * Behavior Mixin
  *
- * Behaviors are attached in FIFO order during construction to allow to allow a behavior that is added by
- * a sub class to remix a previously mixed method to one of it's own methods.
+ * Behaviors are attached in FIFO order during construction. Behaviors are added by name and, at runtime behaviors
+ * cannot be overridden by attaching a behaviors with the same.
  *
  * @author  Johan Janssens <http://github.com/johanjanssens>
  * @package Koowa\Library\Command
@@ -46,10 +46,10 @@ class KBehaviorMixin extends KCommandMixin
         //Set the auto mixin state
         $this->_auto_mixin = $config->auto_mixin;
 
-        //Add the behaviors in FIFO order (allow behavior remixing).
+        //Add the behaviors in FIFO order
         $behaviors = (array) KObjectConfig::unbox($config->behaviors);
 
-        foreach (array_reverse($behaviors) as $key => $value)
+        foreach ($behaviors as $key => $value)
         {
             if (is_numeric($key)) {
                 $this->attachBehavior($value);
@@ -125,58 +125,53 @@ class KBehaviorMixin extends KCommandMixin
      */
     public function attachBehavior($behavior, $config = array())
     {
-        if (!($behavior instanceof KBehaviorInterface)) {
-            $behavior = $this->createBehavior($behavior, $config);
+        //Get the behavior identifier
+        if (!($behavior instanceof KBehaviorInterface))
+        {
+            if (!($behavior instanceof KObjectIdentifier))
+            {
+                //Create the complete identifier if a partial identifier was passed
+                if (is_string($behavior) && strpos($behavior, '.') === false)
+                {
+                    $identifier = clone $this->getIdentifier();
+                    $identifier->path = array($identifier->path[0], 'behavior');
+                    $identifier->name = $behavior;
+                }
+                else $identifier = $this->getIdentifier($behavior);
+            }
+            else $identifier = $behavior;
         }
+        else $identifier = $behavior->getIdentifier();
 
-        //Store the behavior to allow for name lookups
-        $this->_behaviors[$behavior->getName()] = $behavior;
+        //Attach the behavior if it doesn't exist yet
+        if(!$this->hasBehavior($identifier->name))
+        {
+            //Create the behavior object
+            if (!($behavior instanceof KBehaviorInterface))
+            {
+                $config['mixer'] = $this->getMixer();
+                $behavior = $this->getObject($identifier, $config);
+            }
 
-        //Force set the mixer
-        $behavior->setMixer($this->getMixer());
+            if (!($behavior instanceof KBehaviorInterface)) {
+                throw new UnexpectedValueException("Behavior $identifier does not implement KBehaviorInterface");
+            }
 
-        //Enqueue the behavior
-        $this->getCommandChain()->enqueue($behavior);
+            //Store the behavior to allow for name lookups
+            $this->_behaviors[$behavior->getName()] = $behavior;
 
-        //Mixin the behavior
-        if ($this->_auto_mixin) {
-            $this->mixin($behavior);
+            //Force set the mixer
+            $behavior->setMixer($this->getMixer());
+
+            //Enqueue the behavior
+            $this->getCommandChain()->enqueue($behavior);
+
+            //Mixin the behavior
+            if ($this->_auto_mixin) {
+                $this->mixin($behavior);
+            }
         }
 
         return $this->getMixer();
-    }
-
-    /**
-     * Create a behavior by identifier
-     *
-     * @param   mixed   $behavior  An ObjectIdentifier or a valid identifier string
-     * @param   array   $config    An optional associative array of configuration settings
-     * @throws \UnexpectedValueException    If the behavior does not implement the KBehaviorInterface
-     * @return KBehaviorInterface
-     */
-    public function createBehavior($behavior, $config = array())
-    {
-        if (!($behavior instanceof KObjectIdentifier))
-        {
-            //Create the complete identifier if a partial identifier was passed
-            if (is_string($behavior) && strpos($behavior, '.') === false)
-            {
-                $identifier = clone $this->getIdentifier();
-                $identifier->path = array($identifier->path[0], 'behavior');
-                $identifier->name = $behavior;
-            }
-            else $identifier = $this->getIdentifier($behavior);
-        }
-        else $identifier = $behavior;
-
-        //Create the behavior object
-        $config['mixer'] = $this->getMixer();
-        $behavior = $this->getObject($identifier, $config);
-
-        if (!($behavior instanceof KBehaviorInterface)) {
-            throw new UnexpectedValueException("Behavior $identifier does not implement KBehaviorInterface");
-        }
-
-        return $behavior;
     }
 }
