b5bbf491f348d39c4aca7c05673b56e9b025c3a3
Johan Janssens <johan@timble.net>
2013-12-13 00:35:40 +0100

    re #91 : Added KUserProvider interface

---
+++ code/libraries/koowa/libraries/user/provider/interface.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Koowa Framework - http://developer.joomlatools.com/koowa
+ *
+ * @copyright	Copyright (C) 2007 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://github.com/joomlatools/koowa for the canonical source repository
+ */
+
+/**
+ * User Provider Interface
+ *
+ * A user provider is capable of loading and instantiation KUserInterface objects from a backend.
+ *
+ * In a typical authentication configuration, a username (i.e. some unique user identifier) credential enters the system
+ * (via form login, or any method). The user provider that is configured with that authentication method is asked to fetch
+ * the KUserInterface object for the given username or identifier.
+ *
+ * Internally, a user provider can load users from any source (databases, configuration, web service). This is totally
+ * independent of how the authentication information is submitted or what the KUserInterface object looks like.
+ *
+ * @author  Johan Janssens <https://github.com/johanjanssens>
+ * @package Koowa\Library\User
+ */
+interface KUserProviderInterface
+{
+    /**
+     * Loads the user for the given username or identifier
+     *
+     * @param string $username The username
+     * @return KUserInterface|null Returns a KUserInterface object or NULL if the user could not be found.
+     */
+    public function load($username);
+
+    /**
+     * Refreshes the user for the account interface.
+     *
+     * It is up to the implementation to decide if the user data should be totally reloaded (e.g. from the database), or
+     * if the KUserInterface object can just be merged into some internal array of users / identity map.
+     *
+     * @param KUserInterface $user
+     * @return KUserInterface
+     */
+    public function refresh(KUserInterface $user);
+}
