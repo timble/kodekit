0645610673d9c6e52470bb8d202e89a35a28aac2
Johan Janssens <johan@timble.net>
2013-12-19 17:43:25 +0100

    re #91 : Added KTemplateFilterMeta

---
+++ code/libraries/koowa/libraries/template/filter/meta.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Koowa Framework - http://developer.joomlatools.com/koowa
+ *
+ * @copyright	Copyright (C) 2007 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://github.com/joomlatools/koowa for the canonical source repository
+ */
+
+/**
+ * Meta Template Filter
+ *
+ * Filter to parse meta tags
+ *
+ * @author  Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @package Nooku\Library\Template
+ */
+class KTemplateFilterMeta extends KTemplateFilterTag
+{
+	/**
+	 * Parse the text for script tags
+	 *
+	 * @param string $text  The text to parse
+	 * @return string
+	 */
+	protected function _parseTags(&$text)
+	{
+		$tags = '';
+
+		$matches = array();
+		if(preg_match_all('#<meta\ content="([^"]+)"(.*)\/>#iU', $text, $matches))
+		{
+			foreach(array_unique($matches[1]) as $key => $match)
+			{
+                //Set required attributes
+                $attribs = array(
+                    'content' => $match
+                );
+
+                $attribs = array_merge($this->parseAttributes( $matches[2][$key]), $attribs);
+				$tags .= $this->_renderTag($attribs);
+			}
+
+			$text = str_replace($matches[0], '', $text);
+		}
+
+		return $tags;
+	}
+
+    /**
+     * Render the tag
+     *
+     * @param 	array	$attribs Associative array of attributes
+     * @param 	string	$content The tag content
+     * @return string
+     */
+    protected function _renderTag($attribs = array(), $content = null)
+	{
+		$attribs = $this->buildAttributes($attribs);
+
+		$html = '<meta '.$attribs.' />'."\n";
+		return $html;
+	}
+}
