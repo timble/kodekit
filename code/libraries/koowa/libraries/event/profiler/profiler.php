ac857c0248da66aef59eb8b87a322c1814193b32
Johan Janssens <johan@timble.net>
2013-12-21 14:05:40 +0100

    Docblocks

--- code/libraries/koowa/libraries/event/profiler/profiler.php
+++ code/libraries/koowa/libraries/event/profiler/profiler.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * Nooku Framework - http://www.nooku.org
+ * Koowa Framework - http://developer.joomlatools.com/koowa
  *
- * @copyright	Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @copyright	Copyright (C) 2007 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		git://git.assembla.com/nooku-framework.git for the canonical source repository
+ * @link		http://github.com/joomlatools/koowa for the canonical source repository
  */
 
 /**
  * Event Profiler
  *
- * @author  Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @author  Johan Janssens <https://github.com/johanjanssens>
  * @package Nooku\Library\Event
  */
 class KEventProfiler extends KObjectDecorator implements KEventProfilerInterface
