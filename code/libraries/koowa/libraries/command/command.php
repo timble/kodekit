b5a279eeca3a7c28592f4e4c25f6c53917621282
Johan Janssens <johan@timble.net>
2014-01-20 01:25:40 +0100

    re #91 : Added KCommandInterface::setAttributes(), getAttributes(), setAttribute() and getAttribute() methods. KCommand constructor now accepts optional attributes and subject parameters.

--- code/libraries/koowa/libraries/command/command.php
+++ code/libraries/koowa/libraries/command/command.php
@@ -16,24 +16,138 @@
 class KCommand extends KObjectConfig implements KCommandInterface
 {
     /**
+     * The event name
+     *
+     * @var array
+     */
+    protected $_name;
+
+    /**
+     * Subject of the command
+     *
+     * @var mixed
+     */
+    protected $_subject;
+
+    /**
+     * Constructor.
+     *
+     * @param  string              $name       The command name
+     * @param  array|Traversable   $attributes An associative array or a Traversable object instance
+     * @param  mixed               $subject    The command subject
+     */
+    public function __construct($name = '', $attributes = array(), $subject = null)
+    {
+        parent::__construct($attributes);
+
+        $this->setName($name);
+        $this->setSubject($subject);
+    }
+
+    /**
+     * Get the event name
+     *
+     * @return string	The event name
+     */
+    public function getName()
+    {
+        return $this->_name;
+    }
+
+    /**
+     * Set the event name
+     *
+     * @param string $name  The event name
+     * @return KCommand
+     */
+    public function setName($name)
+    {
+        $this->_name = $name;
+        return $this;
+    }
+
+    /**
      * Get the command subject
      *
-     * @return object	The command subject
+     * @return mixed The command subject
      */
     public function getSubject()
     {
-        return $this->get('subject');
+        return $this->_subject;
     }
 
     /**
      * Set the command subject
      *
-     * @param KObjectInterface $subject The command subject
-     * @return $this
+     * @param mixed $subject The command subject
+     * @return KCommand
      */
-    public function setSubject(KObjectInterface $subject)
+    public function setSubject($subject)
     {
-        $this->set('subject', $subject);
+        $this->_subject = $subject;
+        return $this;
+    }
+
+    /**
+     * Set attributes
+     *
+     * Overwrites existing attributes
+     *
+     * @param  array|Traversable $attributes
+     * @throws InvalidArgumentException If the attributes are not an array or are not traversable.
+     * @return KCommand
+     */
+    public function setAttributes($attributes)
+    {
+        if (!is_array($attributes) || $attributes instanceof Traversable)
+        {
+            throw new InvalidArgumentException(sprintf(
+                'Event arguments must be an array or an object implementing the Traversable interface; received "%s"', gettype($attributes)
+            ));
+        }
+
+        //Set the arguments.
+        foreach ($attributes as $key => $value) {
+            $this->set($key, $value);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get all arguments
+     *
+     * @return array
+     */
+    public function getAttributes()
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Get an attribute
+     *
+     * If the attribute does not exist, the $default value will be returned.
+     *
+     * @param  string $name The attribute name
+     * @param  mixed $default
+     * @return mixed
+     */
+    public function getAttribute($name, $default = null)
+    {
+        return $this->get($name, $default);
+    }
+
+    /**
+     * Set an attribute
+     *
+     * @param  string $name The attribute
+     * @param  mixed $value
+     * @return KCommand
+     */
+    public function setAttribute($name, $value)
+    {
+        $this->set($name, $value);
         return $this;
     }
 
@@ -47,17 +161,20 @@ class KCommand extends KObjectConfig implements KCommandInterface
     public function set($name, $value)
     {
         if (is_array($value)) {
-            $this->_data[$name] = new KObjectConfig($value);
-        } else {
-            $this->_data[$name] = $value;
+            $value = new KObjectConfig($value);
         }
+
+        parent::set($name, $value);
     }
 
     /**
-     * Get a command property
+     * Get a command property or attribute
+     *
+     * If a command property exists the property will be returned, otherwise the attribute will be returned. If no
+     * property or attribute can be found the method will return NULL.
      *
      * @param  string $name
-     * @return mixed  The property value
+     * @return mixed|null  The property value
      */
     public function __get($name)
     {
@@ -72,7 +189,9 @@ class KCommand extends KObjectConfig implements KCommandInterface
     }
 
     /**
-     * Set a command property
+     * Set a command property or attribute
+     *
+     * If a command property exists the property will be set, otherwise an attribute will be added.
      *
      * @param  string $name
      * @param  mixed  $value
