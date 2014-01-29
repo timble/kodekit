e2155985c3e250c0638bcf56d933c1cd9a92f8f6
Stian Didriksen <stian@timble.net>
2014-01-29 14:28:05 +0100

    re #631 working on datepicker fix

---
+++ code/media/koowa/com_koowa/js/koowa.datepicker.js
@@ -0,0 +1,29 @@
+/*
+ ---
+
+ description: Custom configuration of bootstrap-datepicker tuned for Koowa
+
+ authors:
+ - Stian Didriksen
+
+ requires:
+ - bootstrap-datepicker
+
+ license: GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+
+ copyright: Copyright (C) 2007 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+
+ ...
+ */
+
+(function ($) {
+    "use strict";
+
+    $.fn.koowaDatepicker = function (options) {
+
+        //@TODO call datepicker plugin
+
+        return this;
+    };
+
+})(kQuery);
