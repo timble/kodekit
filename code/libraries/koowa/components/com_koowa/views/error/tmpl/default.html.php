f076a5280435b59c97c8ba4cd1f56d65ceb7957d
Johan Janssens <johan@timble.net>
2014-07-17 04:03:52 +0200

    re #138 : Cleanup
    - Remove page_data
    - Use script and style elements.

--- code/libraries/koowa/components/com_koowa/views/error/tmpl/default.html.php
+++ code/libraries/koowa/components/com_koowa/views/error/tmpl/default.html.php
@@ -9,14 +9,11 @@
 
 defined('KOOWA') or die; ?>
 
-<?php
-$document = JFactory::getDocument();
-$document->addScript('/media/koowa/com_koowa/js/debugger.js');
-?>
 
 <title content="replace"><?= @translate('Error').' '.$code.' - '. KHttpResponse::$status_messages[$code];; ?></title>
 
-<link type="text/css" rel="stylesheet" href="/media/koowa/com_koowa/css/debugger.css" media="" attribs="" type="text/css" />
+<style src="media://koowa/com_koowa/css/debugger.css" />
+<script src="media://koowa/com_koowa/js/debugger.js" />
 
 <!--[if IE 8]>
 <div class="old-ie">
@@ -28,109 +25,9 @@ $document->addScript('/media/koowa/com_koowa/js/debugger.js');
         <span class="page_header__exception"><?= $exception ?></span>
         <span class="page_header__code">[<?= $code ?>]</span>
     </h1>
+
     <div class="page_message">
         <div class="page_message__text"><?= $message ?></div>
-        <div id="page_data__button" class="page_message__button">Show page data</div>
-    </div>
-
-    <div id="page_data" class="page_data">
-        <h2>Server/Request Data</h2>
-        <table class="data-table">
-            <thead>
-                <tr>
-                    <td class="data-table-k">Key</td>
-                    <td class="data-table-v">Value</td>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>DOCUMENT_ROOT</td>
-                    <td>/demo/dev/whoops/examples</td>
-                </tr>
-                <tr>
-                    <td>REMOTE_ADDR</td>
-                    <td>127.0.0.1</td>
-                </tr>
-                <tr>
-                    <td>REMOTE_PORT</td>
-                    <td>42317</td>
-                </tr>
-                <tr>
-                    <td>SERVER_SOFTWARE</td>
-                    <td>PHP 5.4.6-1ubuntu1.2 Development Server</td>
-                </tr>
-                <tr>
-                    <td>SERVER_PROTOCOL</td>
-                    <td>HTTP/1.1</td>
-                </tr>
-                <tr>
-                    <td>SERVER_NAME</td>
-                    <td>localhost</td>
-                </tr>
-                <tr>
-                    <td>SERVER_PORT</td>
-                    <td>8080</td>
-                </tr>
-                <tr>
-                    <td>REQUEST_URI</td>
-                    <td>/example-silex.php</td>
-                </tr>
-                <tr>
-                    <td>REQUEST_METHOD</td>
-                    <td>GET</td>
-                </tr>
-                <tr>
-                    <td>SCRIPT_NAME</td>
-                    <td>/example-silex.php</td>
-                </tr>
-                <tr>
-                    <td>SCRIPT_FILENAME</td>
-                    <td>/demo/dev/whoops/examples/example-silex.php</td>
-                </tr>
-                <tr>
-                    <td>PHP_SELF</td>
-                    <td>/example-silex.php</td>
-                </tr>
-                <tr>
-                    <td>HTTP_HOST</td>
-                    <td>localhost:8080</td>
-                </tr>
-                <tr>
-                    <td>HTTP_CONNECTION</td>
-                    <td>keep-alive</td>
-                </tr>
-                <tr>
-                    <td>HTTP_ACCEPT</td>
-                    <td>text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8</td>
-                </tr>
-                <tr>
-                    <td>HTTP_USER_AGENT</td>
-                    <td>Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.22 (KHTML, like Gecko) Ubuntu Chromium/25.0.1364.160
-                        Chrome/25.0.1364.160 Safari/537.22
-                    </td>
-                </tr>
-                <tr>
-                    <td>HTTP_ACCEPT_ENCODING</td>
-                    <td>gzip,deflate,sdch</td>
-                </tr>
-                <tr>
-                    <td>HTTP_ACCEPT_LANGUAGE</td>
-                    <td>en-US,en;q=0.8</td>
-                </tr>
-                <tr>
-                    <td>HTTP_ACCEPT_CHARSET</td>
-                    <td>ISO-8859-1,utf-8;q=0.7,*;q=0.3</td>
-                </tr>
-                <tr>
-                    <td>REQUEST_TIME_FLOAT</td>
-                    <td>1365585072.0011</td>
-                </tr>
-                <tr>
-                    <td>REQUEST_TIME</td>
-                    <td>1365585072</td>
-                </tr>
-            </tbody>
-        </table>
     </div>
 
     <div class="page_content">
