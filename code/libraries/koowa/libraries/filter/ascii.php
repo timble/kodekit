6e98767db4247992e1abc417dc4f1e96470694c1
Johan Janssens <johan@timble.net>
2014-07-17 18:11:45 +0200

    Revert "re #91 : Add legacy support for ENT_SUBSTITUTE to be forward compatible with PHP 5.4."
    This reverts commit ed68f387696301563c354ff79e5ebf634519894e.

--- code/libraries/koowa/libraries/filter/ascii.php
+++ code/libraries/koowa/libraries/filter/ascii.php
@@ -36,7 +36,7 @@ class KFilterAscii extends KFilterAbstract implements KFilterTraversable
 	 */
 	public function sanitize($value)
 	{
-		$string = htmlentities(utf8_decode($value), ENT_SUBSTITUTE);
+		$string = htmlentities(utf8_decode($value)/*requires PHP5.4, ENT_SUBSTITUTE*/);
 		$string = preg_replace(
 			array('/&szlig;/','/&(..)lig;/', '/&([aouAOU])uml;/','/&(.)[^;]*;/'),
 			array('ss',"$1","$1".'e',"$1"),
