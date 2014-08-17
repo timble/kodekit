e43829c8b0999ad6ec1146d1eb712265e580eaec
Johan Janssens <johan@timble.net>
2014-08-17 15:32:24 +0200

    re #91 : Do not enforce script tag type to text/javascript.

--- code/libraries/koowa/libraries/template/filter/script.php
+++ code/libraries/koowa/libraries/template/filter/script.php
@@ -2,9 +2,9 @@
 /**
  * Nooku Framework - http://nooku.org/framework
  *
- * @copyright	Copyright (C) 2007 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		https://github.com/nooku/nooku-framework for the canonical source repository
+ * @copyright   Copyright (C) 2007 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/nooku/nooku-framework for the canonical source repository
  */
 
 /**
@@ -47,9 +47,7 @@ class KTemplateFilterScript extends KTemplateFilterTag
                     $attribs['type'] = 'text/javascript';
                 };
 
-                if($attribs['type'] == 'text/javascript') {
-                    $tags .= $this->_renderTag($attribs);
-                }
+                $tags .= $this->_renderTag($attribs);
             }
 
             $text = str_replace($matches[0], '', $text);
@@ -67,9 +65,7 @@ class KTemplateFilterScript extends KTemplateFilterTag
                     $attribs['type'] = 'text/javascript';
                 };
 
-                if($attribs['type'] == 'text/javascript') {
-                    $tags .= $this->_renderTag($attribs, $match);
-                }
+                $tags .= $this->_renderTag($attribs, $match);
             }
 
             $text = str_replace($matches[0], '', $text);
