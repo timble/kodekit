f35de39bf5a988974c96d3ecef38e9ab4c567b0a
Ercan Ozkaya <ercan@timble.net>
2014-11-13 12:41:26 +0200

    test #156: Define mb_strlen and mb_substr if they are not available natively

--- code/libraries/koowa/libraries/legacy.php
+++ code/libraries/koowa/libraries/legacy.php
@@ -104,3 +104,118 @@ if (!defined('ENT_SUBSTITUTE'))
         define('ENT_SUBSTITUTE', ENT_IGNORE); //PHP 5.3 behavior
     }
 }
+
+if (!function_exists('mb_strlen'))
+{
+    function mb_strlen($str)
+    {
+        return strlen(utf8_decode($str));
+    }
+}
+
+if (!function_exists('mb_substr'))
+{
+    function mb_substr($str, $offset, $length = NULL)
+    {
+        // generates E_NOTICE
+        // for PHP4 objects, but not PHP5 objects
+        $str = (string)$str;
+        $offset = (int)$offset;
+        if (!is_null($length)) $length = (int)$length;
+
+        // handle trivial cases
+        if ($length === 0) return '';
+        if ($offset < 0 && $length < 0 && $length < $offset)
+            return '';
+
+        // normalise negative offsets (we could use a tail
+        // anchored pattern, but they are horribly slow!)
+        if ($offset < 0) {
+
+            // see notes
+            $strlen = strlen(utf8_decode($str));
+            $offset = $strlen + $offset;
+            if ($offset < 0) $offset = 0;
+
+        }
+
+        $Op = '';
+        $Lp = '';
+
+        // establish a pattern for offset, a
+        // non-captured group equal in length to offset
+        if ($offset > 0) {
+
+            $Ox = (int)($offset/65535);
+            $Oy = $offset%65535;
+
+            if ($Ox) {
+                $Op = '(?:.{65535}){'.$Ox.'}';
+            }
+
+            $Op = '^(?:'.$Op.'.{'.$Oy.'})';
+
+        } else {
+
+            // offset == 0; just anchor the pattern
+            $Op = '^';
+
+        }
+
+        // establish a pattern for length
+        if (is_null($length)) {
+
+            // the rest of the string
+            $Lp = '(.*)$';
+
+        } else {
+
+            if (!isset($strlen)) {
+                // see notes
+                $strlen = strlen(utf8_decode($str));
+            }
+
+            // another trivial case
+            if ($offset > $strlen) return '';
+
+            if ($length > 0) {
+
+                // reduce any length that would
+                // go passed the end of the string
+                $length = min($strlen-$offset, $length);
+
+                $Lx = (int)( $length / 65535 );
+                $Ly = $length % 65535;
+
+                // negative length requires a captured group
+                // of length characters
+                if ($Lx) $Lp = '(?:.{65535}){'.$Lx.'}';
+                $Lp = '('.$Lp.'.{'.$Ly.'})';
+
+            } else if ($length < 0) {
+
+                if ( $length < ($offset - $strlen) ) {
+                    return '';
+                }
+
+                $Lx = (int)((-$length)/65535);
+                $Ly = (-$length)%65535;
+
+                // negative length requires ... capture everything
+                // except a group of  -length characters
+                // anchored at the tail-end of the string
+                if ($Lx) $Lp = '(?:.{65535}){'.$Lx.'}';
+                $Lp = '(.*)(?:'.$Lp.'.{'.$Ly.'})$';
+
+            }
+
+        }
+
+        if (!preg_match( '#'.$Op.$Lp.'#us',$str, $match )) {
+            return '';
+        }
+
+        return $match[1];
+
+    }
+}
