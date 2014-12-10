e892af86b1a9c56b2785c751b5131cfb55c82372
Johan Janssens <johan@timble.net>
2014-12-10 04:16:59 +0100

    Do not add a // slash if the URL doesn't contain a host
     - Add compliance with parse_url() with accepts url's of the format schema:path
     - Allow to parse and write object identifier uri's using as HttpUrl objects.

--- code/libraries/koowa/libraries/http/url/url.php
+++ code/libraries/koowa/libraries/http/url/url.php
@@ -542,28 +542,30 @@ class KHttpUrl extends KObject implements KHttpUrlInterface
      */
     public function toString($parts = self::FULL, $escape = null)
     {
-        $url    = '';
+        $url = '';
         $escape = isset($escape) ? (bool) $escape : $this->getEscape();
 
         //Add the scheme
         if (($parts & self::SCHEME) && !empty($this->scheme)) {
-            $url .= urlencode($this->scheme) . '://';
-        }
-
-        //Add the username and password
-        if (($parts & self::USER) && !empty($this->user))
-        {
-            $url .= urlencode($this->user);
-            if (($parts & self::PASS) && !empty($this->pass)) {
-                $url .= ':' . urlencode($this->pass);
-            }
-
-            $url .= '@';
+            $url .= urlencode($this->scheme) . ':';
         }
 
         // Add the host and port, if any.
         if (($parts & self::HOST) && !empty($this->host))
         {
+            $url .= '//';
+
+            //Add the username and password
+            if (($parts & self::USER) && !empty($this->user))
+            {
+                $url .= urlencode($this->user);
+                if (($parts & self::PASS) && !empty($this->pass)) {
+                    $url .= ':' . urlencode($this->pass);
+                }
+
+                $url .= '@';
+            }
+
             $url .= urlencode($this->host);
 
             if (($parts & self::PORT) && !empty($this->port)) {
