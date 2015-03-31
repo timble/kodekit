63262d1297e73a073ace9d5baa4fe83e1591cbb1
Israel D. Canasa <raeldc@gmail.com>
2015-03-31 17:14:58 +0800

    Set the database name on KDatabaseAdapterAbstrac::__construct
    $this->_options is not an object so accessing ->database  on line 143 results in an error. There was node code that sets $this->_options. So this line seems to be a leftover code and is not needed. Just set the database name on instantiation and put it at $this->_database.

--- code/libraries/koowa/libraries/database/adapter/abstract.php
+++ code/libraries/koowa/libraries/database/adapter/abstract.php
@@ -112,6 +112,9 @@ abstract class KDatabaseAdapterAbstract extends KObject implements KDatabaseAdap
         // Set the table prefix
         $this->_table_needle = $config->table_needle;
 
+        // Set the database name
+        $this->_database = $config->database;
+
         // Mixin the command interface
         $this->mixin('lib:command.mixin', $config);
 
