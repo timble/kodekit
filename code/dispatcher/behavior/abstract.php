37b59df45a425b2e3350849b8fb240f4c8aeec91
Johan Janssens <johan@timble.net>
2015-04-29 00:49:43 +0200

    re #226 : Add KDispatcherBehaviorAbstract

---
+++ code/dispatcher/behavior/abstract.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Nooku Framework - http://nooku.org/framework
+ *
+ * @copyright   Copyright (C) 2007 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/nooku/nooku-framework for the canonical source repository
+ */
+
+/**
+ * Abstract Dispatcher Behavior
+ *
+ * @author  Johan Janssens <http://github.com/johanjanssens>
+ * @package Koowa\Library\Dispatcher\Behavior
+ */
+abstract class KDispatcherBehaviorAbstract extends KControllerBehaviorAbstract
+{
+
+}
