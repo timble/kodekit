6d10341786b24ca5717205b12927a4bc0ae53cd2
Johan Janssens <johan@timble.net>
2016-01-19 00:55:17 +0100

    re #184 : Check if the template object implements TemplateInterface

--- code/template/helper/abstract.php
+++ code/template/helper/abstract.php
@@ -70,6 +70,13 @@ abstract class KTemplateHelperAbstract extends KObject implements KTemplateHelpe
             else $identifier = $this->getIdentifier($this->__template);
 
             $this->__template = $this->getObject($identifier);
+
+            if(!$this->__template instanceof KTemplateInterface)
+            {
+                throw new \UnexpectedValueException(
+                    'Template: '.get_class($this->__template).' does not implement TemplateInterface'
+                );
+            }
         }
 
         return $this->__template;
