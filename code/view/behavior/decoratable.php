3b78a5035633c6755555f81d70be90a15ec6d755
Johan Janssens <johan@timble.net>
2016-05-19 23:45:22 +0200

    Docblocks

--- code/view/behavior/decoratable.php
+++ code/view/behavior/decoratable.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Kodekit Component - http://www.timble.net/kodekit
+ * Kodekit - http://timble.net/kodekit
  *
- * @copyright	Copyright (C) 2007 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
- * @license		MPL v2.0 <https://www.mozilla.org/en-US/MPL/2.0>
- * @link		https://github.com/timble/kodekit-pages for the canonical source repository
- */
+ * @copyright   Copyright (C) 2007 - 2016 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license     MPL v2.0 <https://www.mozilla.org/en-US/MPL/2.0>
+ * @link        https://github.com/timble/kodekit for the canonical source repository
+ *//
 
 namespace  Kodekit\Library;
 
@@ -85,7 +85,7 @@ class ViewBehaviorDecoratable extends ViewBehaviorAbstract
     }
 
     /**
-     * Decorate the module
+     * Decorate the view
      *
      * @param ViewContextInterface $context	A view context object
      * @return void
