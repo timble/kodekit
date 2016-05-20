89cab1b6901b9a91ad58feb946bc87432f71ec5f
Johan Janssens <johan@timble.net>
2016-05-20 03:21:28 +0200

    Fix typo

--- code/view/behavior/decoratable.php
+++ code/view/behavior/decoratable.php
@@ -5,7 +5,7 @@
  * @copyright   Copyright (C) 2007 - 2016 Johan Janssens and Timble CVBA. (http://www.timble.net)
  * @license     MPL v2.0 <https://www.mozilla.org/en-US/MPL/2.0>
  * @link        https://github.com/timble/kodekit for the canonical source repository
- *//
+ */
 
 namespace  Kodekit\Library;
 
