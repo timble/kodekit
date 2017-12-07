d9098ff04180c8ae3e3154fb7bbd7dd198ea81fb
Ercan Ozkaya <ercan@timble.net>
2017-12-07 15:12:01 +0300

    #154: Empty string shouldn’t pass the check

--- code/view/json.php
+++ code/view/json.php
@@ -355,7 +355,7 @@ class ViewJson extends ViewAbstract
             elseif (is_array($value)) {
                 $this->_processLinks($value);
             }
-            elseif (in_array($key, $this->_text_fields)) {
+            elseif (in_array($key, $this->_text_fields, true)) {
                 $array[$key] = $this->_processText($value);
             }
         }
