e4e2d2ba184ec3f4754affad7d181620019ffdd8
Ercan Ozkaya <ercan@timble.net>
2019-05-23 13:10:09 +0300

    joomlatools/joomlatools-framework#269: Do not escape slashes in JSON

--- code/object/config/json.php
+++ code/object/config/json.php
@@ -64,7 +64,7 @@ class ObjectConfigJson extends ObjectConfigFormat
         }
 
         // Encode <, >, ', &, and " for RFC4627-compliant JSON, which may also be embedded into HTML.
-        $data = json_encode($data, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT);
+        $data = json_encode($data, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT | JSON_UNESCAPED_SLASHES);
 
         if($data === false) {
             throw new \DomainException('Cannot encode data to JSON string');
