81f9c20269d4833a1a5b6ec811ef3cfb12202fc8
Ercan Ozkaya <ercan@timble.net>
2019-08-22 12:28:56 +0300

    #209: Fix date

--- code/functions.php
+++ code/functions.php
@@ -2,7 +2,7 @@
 /**
  * Kodekit - http://timble.net/kodekit
  *
- * @copyright   Copyright (C) 2007 - 2016 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @copyright   Copyright (C) 2007 Johan Janssens and Timble CVBA. (http://www.timble.net)
  * @license     MPL v2.0 <https://www.mozilla.org/en-US/MPL/2.0>
  * @link        https://github.com/timble/kodekit for the canonical source repository
  */
