929653a50509c5ee5bf47e5f7b9c99bb89971435
Arunas Mazeika <arunas@timble.net>
2020-10-01 15:55:57 +0200

    #250 Add caching support for authentic requests
    joomlatools/joomlatools-framework #417

--- code/dispatcher/behavior/cacheable.php
+++ code/dispatcher/behavior/cacheable.php
@@ -33,12 +33,13 @@ class DispatcherBehaviorCacheable extends DispatcherBehaviorAbstract
     protected function _initialize(ObjectConfig  $config)
     {
         $config->append(array(
-            'priority' => self::PRIORITY_LOW,
-            'cache'         => true,
-            'cache_private' => false,
-            'cache_time'         => 0, //must revalidate
-            'cache_time_shared'  => 0, //must revalidate proxy,
-            'cache_control'      => array(),
+            'priority'              => self::PRIORITY_LOW,
+            'cache'                 => true,
+            'cache_time'            => 0, //must revalidate
+            'cache_time_private'    => 0, //must revalidate proxy
+            'cache_time_shared'     => 0, //must revalidate proxy,
+            'cache_control'         => [],
+            'cache_control_private' => ['private'],
         ));
 
         parent::_initialize($config);
@@ -59,13 +60,21 @@ class DispatcherBehaviorCacheable extends DispatcherBehaviorAbstract
         if($this->isSupported())
         {
             $response = $mixer->getResponse();
+            $user     = $mixer->getUser();
+
+            if(!$user->isAuthentic())
+            {
+                $cache_control = (array) KObjectConfig::unbox($this->getConfig()->cache_control);
+                $response->setMaxAge($this->getConfig()->cache_time, $this->getConfig()->cache_time_shared);
+            }
+            else
+            {
+                $cache_control = (array) KObjectConfig::unbox($this->getConfig()->cache_control_private);
+                $response->setMaxAge($this->getConfig()->cache_time_private);
+            }
 
             //Reset cache control header (if caching enabled)
-            $cache_control = (array) ObjectConfig::unbox($this->getConfig()->cache_control);
             $response->headers->set('Cache-Control', $cache_control, true);
-
-            //Set max age default
-            $response->setMaxAge($this->getConfig()->cache_time, $this->getConfig()->cache_time_shared);
         }
     }
 
@@ -88,14 +97,10 @@ class DispatcherBehaviorCacheable extends DispatcherBehaviorAbstract
     {
         $request = $this->getRequest();
 
-        $cacheable = false;
-        if($request->isCacheable() && $this->getConfig()->cache)
-        {
+        if($request->isCacheable() && $this->getConfig()->cache) {
             $cacheable = true;
-
-            if(!$this->getConfig()->cache_private && $this->getUser()->isAuthentic()) {
-                $cacheable = false;
-            }
+        } else {
+            $cacheable = false;
         }
 
         return $cacheable;
@@ -116,32 +121,10 @@ class DispatcherBehaviorCacheable extends DispatcherBehaviorAbstract
         $response = $context->response;
         $request  = $context->request;
 
-        if($this->isCacheable())
-        {
-            $response->headers->set('Cache-Control', $this->_getCacheControl());
-
-            //Set Validator
-            $response->setEtag($this->_getEtag(), !$response->isDownloadable());
+        ////Set Etag
+        if($this->isCacheable()) {
+            $response->setEtag($this->_getEtag($response), !$response->isDownloadable());
         }
-    }
-
-    /**
-     * Get the cache control directives
-     *
-     * @link https://tools.ietf.org/html/rfc7234#page-21
-     *
-     * @return array
-     */
-    protected function _getCacheControl()
-    {
-        $response = $this->getResponse();
-        $cache    = $response->getCacheControl();
-
-        if($response->getUser()->isAuthentic()) {
-            $cache[] = 'private';
-        }
-
-        return $cache;
     }
 
     /**
@@ -154,16 +137,14 @@ class DispatcherBehaviorCacheable extends DispatcherBehaviorAbstract
      *
      * @return string
      */
-    protected function _getEtag()
+    protected function _getEtag(HttpResponseInterface $response)
     {
-        $response = $this->getResponse();
-
         if($response->isDownloadable())
         {
             $info = $response->getStream()->getInfo();
             $etag = sprintf('"%x-%x-%s"', $info['ino'], $info['size'],base_convert(str_pad($info['mtime'],16,"0"),10,16));
         }
-        else $etag = hash('crc32b', $response->getContent().$response->getFormat());
+        else $etag = hash('crc32b', $response->getContent().$response->getFormat().$response->getUser()->getId());
         
         return $etag;
     }
