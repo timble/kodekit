b74028009eee4df209066dc13c2f18705c4d24a5
Ercan Ozkaya <ercan@timble.net>
2021-03-26 15:37:21 +0300

    #268: Add PHP8 polyfills

--- code/legacy.php
+++ code/legacy.php
@@ -714,3 +714,24 @@ if (!function_exists('locale_set_default')) {
         return false;
     }
 }
+
+/**
+ * New string functions from PHP 8
+ */
+if (!function_exists('str_starts_with')) {
+    function str_starts_with($haystack, $needle) {
+        return 0 === strncmp($haystack, $needle, strlen($needle));
+    }
+}
+
+if (!function_exists('str_ends_with')) {
+    function str_ends_with($haystack, $needle) {
+        return '' === $needle || ('' !== $haystack && 0 === substr_compare($haystack, $needle, -strlen($needle)));
+    }
+}
+
+if (!function_exists('str_contains')) {
+    function str_contains($haystack, $needle) {
+        return '' === $needle || false !== strpos($haystack, $needle);
+    }
+}
