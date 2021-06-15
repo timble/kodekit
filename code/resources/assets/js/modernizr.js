34b7ab7e3403777bba50189f0d7e46c3eae5ebbb
Ercan Ozkaya <ercan@timble.net>
2021-06-15 13:37:57 +0300

    Sync with kodekit-ui

--- code/resources/assets/js/modernizr.js
+++ code/resources/assets/js/modernizr.js
@@ -1,5 +1,5 @@
 /*!
- * modernizr v3.11.1
+ * modernizr v3.11.3
  * Build https://modernizr.com/download?-appearance-boxshadow-checked-cssanimations-eventlistener-flexbox-flexboxlegacy-flexboxtweener-flexwrap-touchevents-prefixedcss-setclasses-shiv-dontmin-cssclassprefix:k-
  *
  * Copyright (c)
@@ -35,8 +35,7 @@
    * @access public
    */
   var ModernizrProto = {
-    // The current version, dummy
-    _version: '3.11.1',
+    _version: '3.11.3',
 
     // Any settings that don't work as separate modules
     // can go in here as configuration.
